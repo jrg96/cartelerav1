f46f0cf099a6bbeb29cde02f4a375c98fcc17ae0
jrg96 <gomezlopez.jorge96@gmail.com>
2019-12-16 17:41:33 -0600

    Mostrar lista estatica de peliculas

--- cartelerav1/src/main/java/com/cartelerav1/app/controller/HomeController.java
+++ cartelerav1/src/main/java/com/cartelerav1/app/controller/HomeController.java
@@ -1,5 +1,10 @@
 package com.cartelerav1.app.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +15,18 @@ public class HomeController
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String goHome()
 	{
+		return "home";
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String paginaPrincipal(Model model)
+	{
+		List<String> peliculas = new LinkedList<>();
+		peliculas.add("Buscando a Dory");
+		peliculas.add("The Maze Runner");
+		peliculas.add("Los Increibles 2");
+		
+		model.addAttribute("peliculas", peliculas);
 		return "home";
 	}
 }
--- cartelerav1/webapp/WEB-INF/views/home.jsp
+++ cartelerav1/webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,21 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Pagina Principal</title>
+<title>Bienvenido a cineapp</title>
 </head>
 <body>
-	<h1>Plantilla Home</h1>
+	<h1>Lista de peliculas</h1>
+	
+	<ul>
+		<c:forEach items="${peliculas}" var="pelicula">
+			<li>${pelicula}</li>
+		</c:forEach>
+	</ul>
 </body>
 </html>
