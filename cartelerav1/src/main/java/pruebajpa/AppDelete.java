55036d97389f92e8aece9deb283538fb1409a352
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-11 19:13:44 -0600

    Eliminar, Desplegar todas las noticias, usando JPA

---
+++ cartelerav1/src/main/java/pruebajpa/AppDelete.java
@@ -0,0 +1,25 @@
+package pruebajpa;
+
+import java.util.Optional;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cartelerav1.app.model.Noticia;
+import com.cartelerav1.app.repository.NoticiaRepository;
+
+public class AppDelete {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+		
+		NoticiaRepository noticiaRepository = context.getBean("noticiaRepository", NoticiaRepository.class);
+		
+		if(noticiaRepository.existsById(2))
+		{
+			noticiaRepository.deleteById(2);
+		}
+		
+		context.close();
+	}
+
+}
---
+++ cartelerav1/src/main/java/pruebajpa/AppReadAll.java
@@ -0,0 +1,27 @@
+package pruebajpa;
+
+import java.util.Optional;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cartelerav1.app.model.Noticia;
+import com.cartelerav1.app.repository.NoticiaRepository;
+
+public class AppReadAll {
+
+	public static void main(String[] args) {
+ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+		
+		Iterable<Noticia> noticias;
+		NoticiaRepository noticiaRepository = context.getBean("noticiaRepository", NoticiaRepository.class);
+		noticias = noticiaRepository.findAll();
+		
+		for (Noticia noticia : noticias)
+		{
+			System.out.println(noticia.toString());
+		}
+		
+		context.close();
+	}
+
+}
